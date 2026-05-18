ce19c321ca730cf5b73c41bb2612a536472b5dc9
张双双 <1359728589@qq.com>
2026-05-18 13:08:30 +0800

    cros加token黑名单

--- mes-front/src/views/login/Index.vue
+++ mes-front/src/views/login/Index.vue
@@ -5,48 +5,44 @@ import {ref} from 'vue'
 
 let username = ref("")
 let password = ref("")
+let loading = ref(false)  // 只加这一个
 
-// ⭐ 加这一段配置（在 submit 函数之前）
 axios.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
-function submit():void{
-    axios.post("http://localhost:8080/api/v1/auth/login",{
-        username:username.value,
-        password:password.value
-    }).then(res=>{
-        console.log(res.data)
-        if(res.data.code === 200){
-            localStorage.setItem("token",res.data.data.token)
-            router.push({name:"Index"})
-        }
-    })
+function submit(): void {
+  if (loading.value) return;  // 防重复提交
+  loading.value = true;
+
+  axios.post("http://localhost:8080/api/v1/auth/login", {
+    username: username.value,
+    password: password.value
+  }).then(res => {
+    console.log(res.data)
+    if (res.data.code === 200) {
+      localStorage.setItem("token", res.data.data.token)
+      router.push({name: "Index"})
+    }
+  }).finally(() => {
+    loading.value = false;  // 重置loading
+  })
 }
 </script>
 
 <template>
-    <form
-        @submit.prevent="submit"
-    >
-        <input
-            v-model="username"
-            placeholder="请输入用户名"
-            placeholder-class="input-placeholder"
-        />
-        <input
-            v-model="password"
-            placeholder="请输入密码"
-            placeholder-class="input-placeholder"
-            
-        />
-        <button form-type="submit">提交</button>
-        <button form-type="reset">重置</button>
-    </form>
+  <form @submit.prevent="submit">
+    <input v-model="username" placeholder="请输入用户名" />
+    <input v-model="password" type="password" placeholder="请输入密码" />
+    <button type="submit" :disabled="loading">
+      {{ loading ? "登录中..." : "提交" }}
+    </button>
+    <button type="reset">重置</button>
+  </form>
 </template>
 
 
