5f86fe359e1a09986c75bfa1048bccac162a167f
懒洋洋大王 <1359728589@qq.com>
2026-05-29 11:45:26 +0800

    sql

---
+++ sql/1.sql
@@ -0,0 +1,125 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 8.0.37 : Database - mes
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mes` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `mes`;
+
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `permission_code` varchar(100) NOT NULL COMMENT '权限代码',
+  `permission_name` varchar(100) NOT NULL COMMENT '权限名称',
+  `parent_id` int DEFAULT '0' COMMENT '父权限ID',
+  `type` tinyint DEFAULT '1' COMMENT '类型:1-菜单,2-按钮,3-API',
+  `url` varchar(225) DEFAULT NULL COMMENT 'API路径',
+  `method` varchar(10) DEFAULT NULL COMMENT '请求方法',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_permission_code` (`permission_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_permission` */
+
+insert  into `sys_permission`(`id`,`permission_code`,`permission_name`,`parent_id`,`type`,`url`,`method`) values 
+(1,'user:view','查看用户',0,3,'/api/v1/auth/online-users','GET'),
+(2,'user:kick','踢用户下线',0,3,'/api/v1/auth/kick/*','DELETE'),
+(3,'self:logout','退出登录',0,3,'/api/v1/auth/logout','POST'),
+(4,'self:kick-self','踢自己下线',0,3,'/api/v1/auth/kick-self','DELETE'),
+(5,'self:info','查看自己信息',0,3,'/api/v1/auth/current-user','GET');
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_code` varchar(50) NOT NULL COMMENT '角色代码',
+  `role_name` varchar(50) NOT NULL COMMENT '角色名称',
+  `description` varchar(225) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_role_code` (`role_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`id`,`role_code`,`role_name`,`description`) values 
+(1,'ADMIN','管理员','拥有所有权限'),
+(2,'USER','普通用户','只有基本权限');
+
+/*Table structure for table `sys_role_permission` */
+
+DROP TABLE IF EXISTS `sys_role_permission`;
+
+CREATE TABLE `sys_role_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `permission_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_role_id` (`role_id`),
+  KEY `idx_permission_id` (`permission_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_role_permission` */
+
+insert  into `sys_role_permission`(`id`,`role_id`,`permission_id`) values 
+(1,1,5),
+(2,1,4),
+(3,1,3),
+(4,1,2),
+(5,1,1),
+(8,2,5),
+(9,2,4),
+(10,2,3);
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `status` tinyint DEFAULT '1' COMMENT '状态:0-禁用,1-启用',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_user` */
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_role_id` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_user_role` */
+
+insert  into `sys_user_role`(`id`,`user_id`,`role_id`) values 
+(1,1,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
