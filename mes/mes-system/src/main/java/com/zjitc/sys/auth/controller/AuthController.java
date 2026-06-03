3c6fc93b193a32a74c076c052175819146df2623
张双双 <1359728589@qq.com>
2026-06-03 16:58:04 +0800

    前端

--- mes/mes-system/src/main/java/com/zjitc/sys/auth/controller/AuthController.java
+++ mes/mes-system/src/main/java/com/zjitc/sys/auth/controller/AuthController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
-@CrossOrigin(origins = "*", allowCredentials = "true")
+@CrossOrigin(originPatterns = "*", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
